1a5191871d28bf6c8bf3b9ca0c699364e1a310df
saimageshvar <saimageshvar@gmail.com>
2017-01-06 17:16:29 +0530

    index page changes

--- cerebra/index.php
+++ cerebra/index.php
@@ -36,7 +36,7 @@ if (!isset($_SESSION['user']))
     <div class="row">
     </div>  
   </nav>
-  <div class="container">
+  <div class="container" style="margin-top: 15vh; margin-bottom: 10vh">
     <div class="section">
       <div class="row">
        <div class="col s12 m6 push-m3 l6 push-l3">
@@ -68,14 +68,14 @@ if (!isset($_SESSION['user']))
    </div>
  </div>
 </div>
-<footer class="page-footer">
+<!-- <footer class="page-footer teal darken-2" style="padding-top: 0px">
   <div class="footer-copyright">
     <div class="container">
       © 2014 Copyright Text
       <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
     </div>
   </div>
-</footer>
+</footer> -->
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
 <script type="text/javascript" src="js/utils.js"></script>
