0cf853cababc0a93e7d6e946e99f8b829891dfb6
saimageshvar <saimageshvar@gmail.com>
2017-01-07 20:26:28 +0530

    gameplay header

--- cerebra/GamePlay.php
+++ cerebra/GamePlay.php
@@ -81,129 +81,123 @@ if (isset($_SESSION['user']))
 		</head>
 
 		<body style="overflow-x: hidden;">
-			<nav class="top-nav teal darken-2" style="min-height: 100px">
+			<nav class="top-nav teal darken-2" style="height: 80px">
 				<div class="nav-wrapper">
-					<div class="row">
-						<div class="col s12 m4">
-							<a href="//kurukshetra.org.in" class="brand-logo"><img class="responsive-img" src="img/k_logo.png" style="width: 250px"></a>
-						</div>
-						<div class="col s6 m5"class="brand-logo">
-							<p class="flow-text">MAIN RUN</p>
-						</div>
-						<div class="col s12 m2">
-							<p class="brand-logo" id="timer" ></p>
-						</div>
-						<div class="col s6 m1 right">
-							<a class="brand-logo" href="logout.php" style="padding-top: 30px;"><i class="large material-icons" style="font-size: 35px;">power_settings_new</i></a>
-							<!--a >Logout</a-->
-						</div>
-					</div>
+					<a href="//kurukshetra.org.in" class="brand-logo"><img class="responsive-img" src="img/k_logo.png" style="width: 200px"></a>
+					<a href="#" class="brand-logo right hide-on-med-and-down" style="padding-top: 20px"><img class="responsive-img" src="img/ceg.png" style="width: 200px"></a>
+
 				</div>
 			</nav>
 			<main>
-				<div class="container">
+				<div class="container" style="padding-top: 20px">
+					<div class="row">
+						<div class="col s6">
+							<a class="waves-effect waves-light btn" id="timer"></a>
+						</div>
+						<div class="col s6">
+							<a class="waves-effect waves-light btn" id="timer">12:03</a>
+						</div>					</div>
+						<div class="row" style="padding-top:80px;">
+							<ul class="tabs" >
+								<li class="tab col s12 l4"><a class="active" href="#game" style="font-size:18px" >Game Play</a></li>
+								<li class="tab col s12 l4"><a href="#lb" style="font-size:18px" onclick="getLeaderboard();">Leaderboard</a></li>
+								<p class="right col s12 l4 points" id="points" style="font-size: 25px; margin-top: 5px;">Points: <?php echo $_SESSION['user']['points'] ?></p>
+							</ul>
 
-					<div class="row" style="padding-bottom:40px;">
-						<ul class="tabs" >
-							<li class="tab col s12 l4"><a class="active" href="#game" style="font-size:18px" >Game Play</a></li>
-							<li class="tab col s12 l4"><a href="#lb" style="font-size:18px" onclick="getLeaderboard();">Leaderboard</a></li>
-							<p class="right col s12 l4 points" id="points" style="font-size: 25px; margin-top: 5px;">Points: <?php echo $_SESSION['user']['points'] ?></p>
-						</ul>
+							<div id="game" class="col s12" align="center">
+								<ul class="collapsible popout" data-collapsible="accordion" style="width:100%; display: inline-block; text-align: left">
 
-						<div id="game" class="col s12" align="center">
-							<ul class="collapsible popout" data-collapsible="accordion" style="width:100%; display: inline-block; text-align: left">
-
-								<?php 
-								$j = 0; $count =  0;
-								for($i=0;$i<$_SESSION['user']['state'] ; $i++) { ?>
-								<li>
-									<!-- SET begins -->
-									<div class="collapsible-header teal lighten-5" style="padding-bottom:10px;min-height: 4em; line-height: 4em; font-weight:bold; font-size: 20px; text-align:center">
-										SET <?php echo $i+1 ?>
-									</div>
-									<div class="collapsible-body">		 	
-										<div class="row">
-											<br>
-											<?php								
+									<?php 
+									$j = 0; $count =  0;
+									for($i=0;$i<$_SESSION['user']['state'] ; $i++) { ?>
+									<li>
+										<!-- SET begins -->
+										<div class="collapsible-header teal lighten-5" style="padding-bottom:10px;min-height: 4em; line-height: 4em; font-weight:bold; font-size: 20px; text-align:center">
+											SET <?php echo $i+1 ?>
+										</div>
+										<div class="collapsible-body">		 	
+											<div class="row">
+												<br>
+												<?php								
 	  // number of questions in each set
-											for($k=0;$k<(sizeof($_SESSION['questions'])/$_SESSION['user']['state']);$k++)
-											{
+												for($k=0;$k<(sizeof($_SESSION['questions'])/$_SESSION['user']['state']);$k++)
+												{
 									//echo $j ; 
-												if(!in_array($_SESSION['questions'][$j]['key'],$_SESSION['questions_answered']))
-												{									 
-													$count = 1;
-													?>
-													<div class="col s8 offset-s2">
-														<div class="card hoverable grey lighten-4">
-															<div class="card-content" style="padding-bottom: -15px;">
+													if(!in_array($_SESSION['questions'][$j]['key'],$_SESSION['questions_answered']))
+													{									 
+														$count = 1;
+														?>
+														<div class="col s8 offset-s2">
+															<div class="card hoverable grey lighten-4">
+																<div class="card-content" style="padding-bottom: -15px;">
 
-																<div class="col s12" style="font-size:18px;margin-left:5px"><?php echo $_SESSION['questions'][$j]['question']; ?></div>
+																	<div class="col s12" style="font-size:18px;margin-left:5px"><?php echo $_SESSION['questions'][$j]['question']; ?></div>
 
-																<div class="input-field col s11" style="margin-top:0px; margin-left:15px; color:black;">
+																	<div class="input-field col s11" style="margin-top:0px; margin-left:15px; color:black;">
 
-																	<div class="col s12 m9">
-																		<input type="text" placeholder="Your answer" id="answer_<?php echo $_SESSION['questions'][$j]['key'] ?>" class="validate"/>
-																	</div>
-																	<div class="col s6 m2">
-																		<a class="btn-floating btn-large waves-effect waves-light" style="margin-left:5%; margin-bottom: 1%;" id="<?php echo $_SESSION['questions'][$j]['key'] ?>" onclick="submitAnswer(this);"><i class="material-icons">done</i></a>
-																		<div class="progress_loader" id="pl_<?php echo $_SESSION['questions'][$j]['key'] ?>" style="display:none;">Loading...</div>
-																	</div>
-																	<div class="col s6 m1">
-																		<a id="<?php echo $_SESSION['questions'][$j]['key'] ?>" class="btn-floating btn-large waves-effect waves-light black-text blue" onclick="getClue(this);">
-																			<i class="material-icons">lightbulb_outline</i></a>      
-																			<div class="progress_loader" id="clue_<?php echo $_SESSION['questions'][$j]['key'] ?>" style="display:none;">Loading...</div>                
+																		<div class="col s12 m9">
+																			<input type="text" placeholder="Your answer" id="answer_<?php echo $_SESSION['questions'][$j]['key'] ?>" class="validate"/>
+																		</div>
+																		<div class="col s6 m2">
+																			<a class="btn-floating btn-large waves-effect waves-light" style="margin-left:5%; margin-bottom: 1%;" id="<?php echo $_SESSION['questions'][$j]['key'] ?>" onclick="submitAnswer(this);"><i class="material-icons">done</i></a>
+																			<div class="progress_loader" id="pl_<?php echo $_SESSION['questions'][$j]['key'] ?>" style="display:none;">Loading...</div>
+																		</div>
+																		<div class="col s6 m1">
+																			<a id="<?php echo $_SESSION['questions'][$j]['key'] ?>" class="btn-floating btn-large waves-effect waves-light black-text blue" onclick="getClue(this);">
+																				<i class="material-icons">lightbulb_outline</i></a>      
+																				<div class="progress_loader" id="clue_<?php echo $_SESSION['questions'][$j]['key'] ?>" style="display:none;">Loading...</div>                
+																			</div>
+
+																			<!-- <label class="active grey-text text-darken-2" for="first_name2" style="font-size:18px;">Question 1</label>-->
 																		</div>
 
-																		<!-- <label class="active grey-text text-darken-2" for="first_name2" style="font-size:18px;">Question 1</label>-->
+																		<div class="row">
+
+																		</div>
 																	</div>
+																</div>  
+															</div>
 
-																	<div class="row">
-
-																	</div>
-																</div>
-															</div>  
-														</div>
-
-														<?php
+															<?php
+														}
+														$j++;
 													}
-													$j++;
-												}
-												if($count == 0)
-												{
-													?>
-													<p> You have answered all questions in this set.
-														<?php
-													}
-													?>
+													if($count == 0)
+													{
+														?>
+														<p> You have answered all questions in this set.
+															<?php
+														}
+														?>
 
+													</div>
 												</div>
-											</div>
-										</li>
-										<?php
-									}
-									?>
-								</ul>
-							</div>
-							<div id="lb" class="col s12" align="center">
+											</li>
+											<?php
+										}
+										?>
+									</ul>
+								</div>
+								<div id="lb" class="col s12" align="center">
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
+
 			</div>
+		</main>
 
-		</div>
-	</main>
+		<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
 
-	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
-
-	<script type="text/javascript" src="js/utils.js"></script>
+		<script type="text/javascript" src="js/utils.js"></script>
 
 
-	<!-- timer -->
-	<script type="text/javascript" src="js/countdown.min.js"></script>
-	<script type="text/javascript">   
-		$(document).ready(function() {
+		<!-- timer -->
+		<script type="text/javascript" src="js/countdown.min.js"></script>
+		<script type="text/javascript">   
+			$(document).ready(function() {
 		// var startTime = new Date('<?php echo $_SESSION['user']['startTime']; ?>');
 		// var currentTime = new Date('<?php echo $_SESSION['current_time']; ?>');
 		// var diff = currentTime - startTime;
