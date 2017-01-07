e94de77433fe7c46df1dad1e3865e8efa12ad610
Neeharika <neeha2311@gmail.com>
2017-01-07 22:47:56 +0530

    changes in dom

--- cerebra/js/utils.js
+++ cerebra/js/utils.js
@@ -66,7 +66,7 @@ function submitAnswer(e) {
         success: function(result)
         {
             result['data'] = jQuery.parseJSON(result['data']);
-            alert(result['data']['points']);
+            //alert(result['data']['points']);
             if(result['code']==1)
             {
                 Materialize.toast('Right Answer!', 1000);
@@ -74,7 +74,7 @@ function submitAnswer(e) {
                 $(document.getElementById('pl_'+e.id)).hide();
                 $(document.getElementById('clue_'+e.id)).hide();
                 $(e).hide();
-                document.getElementById("points").innerHTML = result['data']['points'];
+                //document.getElementById("points").innerHTML = result['data']['points'];
                 //document.getElementsByClassName("points")[0].append(result['data']['points']);
             }
             else if(result['code']==0)
@@ -82,7 +82,7 @@ function submitAnswer(e) {
                 Materialize.toast('Dai thappudaa!', 1000);
                 $(document.getElementById('pl_'+e.id)).hide();
                 $(e).show();
-                document.getElementById("points").innerHTML = result['data']['points'];
+                //document.getElementById("points").innerHTML = result['data']['points'];
                 //document.getElementsByClassName("points")[0].append(result['data']['points']);
             }
         },
@@ -239,83 +239,83 @@ function getNextLevel() {
                         in7.className = "col s12";
                         in7.style = "font-size:18px; margin-left:5px;";
                         //check
-+                        in7.textContent = result['data'][i]['question'];
-+
-+                        in6.append(in7);
-+
-+                        var in8 = document.createElement('div');
-+                        in8.className = "input-field col s11";
-+                        in8.style = "margin-top:0px; margin-left:15px; color:black;";
-+                        var in9 = document.createElement('div');
-+                        in9.className = "col s12 m9";
-+                        var input = document.createElement('input');    
-+                        //check below 3 statements
-+                        input.type = "text";
-+                        input.placeholder = "Your answer";
-+                        input.id = "answer_"+result['data'][i]['key'];
-+                        input.className = "validate";
-+                        in9.append(input);
-+
-+                        in8.append(in9);
-+
-+                        var in10 = document.createElement('div');
-+                        in10.className = "col s6 m2";
-+                        var in11 = document.createElement('a');
-+                        in11.className = "btn-floating btn-large waves-effect waves-light";
-+                        in11.style = "margin-left:5%; margin-bottom: 1%;"
-+                        in11.id= result['data'][i]['key'];
-+                        in11.onclick = function(){submitAnswer(this);}
-+                        //check
-+                        var button1 = document.createElement('i');
-+                        button1.className = "material-icons";
-+                        button1.textContent = "done";
-+
-+                        in11.append(button1);
-+                        in10.append(in11);
-+
-+                        var pl1 = document.createElement('div');
-+                        pl1.className = "progress_loader";
-+                        pl1.id = result['data'][i]['key'];
-+                        pl1.style = "display:none;"
-+                        pl1.textContent = "Loading...";
-+
-+                        in10.append(pl1);
-+
-+                        in8.append(in10);
-+
-+                        var in12 = document.createElement('div');
-+                        in12.className = "col s6 m1";
-+                        var in13 = document.createElement('a');
-+                        in13.className = "btn-floating btn-large waves-effect waves-light black-text blue";
-+                        in13.id = result['data'][i]['key'];
-+                        in13.onclick = function(){getClue(this);}
-+                        //check
-+                        var button2 = document.createElement('i');
-+                        button2.className = "material-icons";
-+                        button2.textContent = "done";
-+
-+
-+                        in13.append(button2);
-+                        in12.append(in13);
-+
-+                        var pl2 = document.createElement('div');
-+                        pl2.className = "progress_loader";
-+                        pl2.id = "clue_" + result['data'][i]['key'];
-+                        pl2.style = "display:none;"
-+                        pl2.textContent = "Loading...";
-+
-+                        in12.append(pl2);
-+                        in8.append(in12);
-+                        
-+                        in6.append(in8);    
-+                        
-+                        var in14 = document.createElement('div');
-+                        in14.className = "row";
-+                        in6.append(in14);
-+                        in5.append(in6);
-+                        in4.append(in5);
-+                        in3.append(in4); 
-+                    }
+                        in7.textContent = result['data'][i]['question'];
+
+                        in6.append(in7);
+
+                        var in8 = document.createElement('div');
+                        in8.className = "input-field col s11";
+                        in8.style = "margin-top:0px; margin-left:15px; color:black;";
+                        var in9 = document.createElement('div');
+                        in9.className = "col s12 m9";
+                        var input = document.createElement('input');    
+                        //check below 3 statements
+                        input.type = "text";
+                        input.placeholder = "Your answer";
+                        input.id = "answer_"+result['data'][i]['key'];
+                        input.className = "validate";
+                        in9.append(input);
+
+                        in8.append(in9);
+
+                        var in10 = document.createElement('div');
+                        in10.className = "col s6 m2";
+                        var in11 = document.createElement('a');
+                        in11.className = "btn-floating btn-large waves-effect waves-light";
+                        in11.style = "margin-left:5%; margin-bottom: 1%;"
+                        in11.id= result['data'][i]['key'];
+                        in11.onclick = function(){submitAnswer(this);}
+                        //check
+                        var button1 = document.createElement('i');
+                        button1.className = "material-icons";
+                        button1.textContent = "done";
+
+                        in11.append(button1);
+                        in10.append(in11);
+
+                        var pl1 = document.createElement('div');
+                        pl1.className = "progress_loader";
+                        pl1.id = result['data'][i]['key'];
+                        pl1.style = "display:none;"
+                        pl1.textContent = "Loading...";
+
+                        in10.append(pl1);
+
+                        in8.append(in10);
+
+                        var in12 = document.createElement('div');
+                        in12.className = "col s6 m1";
+                        var in13 = document.createElement('a');
+                        in13.className = "btn-floating btn-large waves-effect waves-light black-text blue";
+                        in13.id = result['data'][i]['key'];
+                        in13.onclick = function(){getClue(this);}
+                        //check
+                        var button2 = document.createElement('i');
+                        button2.className = "material-icons";
+                        button2.textContent = "done";
+
+
+                        in13.append(button2);
+                        in12.append(in13);
+
+                        var pl2 = document.createElement('div');
+                        pl2.className = "progress_loader";
+                        pl2.id = "clue_" + result['data'][i]['key'];
+                        pl2.style = "display:none;"
+                        pl2.textContent = "Loading...";
+
+                        in12.append(pl2);
+                        in8.append(in12);
+                        
+                        in6.append(in8);    
+                        
+                        var in14 = document.createElement('div');
+                        in14.className = "row";
+                        in6.append(in14);
+                        in5.append(in6);
+                        in4.append(in5);
+                        in3.append(in4); 
+                    }
 
                 in2.append(in3);
                 outer.append(in1);
